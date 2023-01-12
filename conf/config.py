cb55eb724e9e6ec670531ce753ba5906a1645d79
qiuzixuan <zixuan.qiu@ronds.com.cn>
2023-01-12 17:32:44 +0800

    1. 去除 .secrets.toml

--- conf/config.py
+++ conf/config.py
@@ -10,13 +10,12 @@ project_path = GetPath('conf')
 
 settings = Dynaconf(
     settings_files=[
-        project_path.get_project_path('.secrets.toml'),
         project_path.get_project_path('settings.toml')
     ],
 )
 
 if __name__ == '__main__':
-    test_func = "single211"
+    test_func = "s211"
 
     send_msg = getattr(settings, test_func)
     print(send_msg.api_url)
